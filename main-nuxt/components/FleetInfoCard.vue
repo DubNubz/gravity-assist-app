a93a699fe0724a96fac68e0e9c19b3fcd995ccad
kennethn23 <145921389+DubNubz@users.noreply.github.com>
2024-05-31 00:06:47 -0400

    descriptions

--- main-nuxt/components/FleetInfoCard.vue
+++ main-nuxt/components/FleetInfoCard.vue
@@ -1,14 +1,24 @@
 <template>
-    <div class="outer" v-if="fleetStore().preset">
+    <div class="outer" v-if="currentType">
         <div class="infoCard">
-            <h2>{{ fleetStore().preset }}</h2>
+            <h2>{{ currentType }}</h2>
+            <h3>{{ descriptions[currentType] }}</h3>
+            <button><h2>Create <span v-if="currentType != 'Custom'">with template</span></h2></button>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
 
+const descriptions: Record<string, string> = {
+    "Swarm": "Swarm fleets are fleets that hold enough aircraft to block out the sun. Aircraft losses are expected and must be rebuilt frequently.",
+    "Tank": "Tank fleets are fleets that absorb damage from enemy fleets while buying time for allies to flank.",
+    "DPS": "DPS fleets are fleets that are built to do as much damage as possible. They can deal significant amounts of damage to enemies if left unaccounted.",
+    "Custom": "Plan out and create a fleet with any ship or aircraft!"
+}
+const currentType = ref(fleetStore().preset);
 
+watch(() => fleetStore().preset, () => currentType.value = fleetStore().preset);
 
 </script>
 
